93cd89f2451c034724701d38603c7ccf3a02ea3f
Yash-Pawar-2131 <yashpawar2131@gmail.com>
2023-03-20 18:56:46 +0530

    xy plane movement

--- scripts/Proto_Drone2.gd
+++ scripts/Proto_Drone2.gd
@@ -1,14 +1,40 @@
 extends RigidBody
 
+onready var desired_drone_state = get_node("/root/Spatial/Desired_Drone_State")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const SPEED = 1
+
+const PGAIN = 20
+const DGAIN = 3
+const IGAIN = 2
+
+var previous_error : float
+var error : float
+var error_derivative : float
+var error_integral : float
+var force : Vector3
+
+var thrust : float
+var yaw : float
+var pitch : float
+var roll : float
+
+var motorFR : float
+var motorFL : float
+var motorBR : float
+var motorBL : float
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	previous_error = 0.0
+	error_integral = 0.0
+
+	thrust = 0.0
+	yaw = 0.0
+	pitch = 0.0
+	roll = 0.0
+
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,12 +42,24 @@ func _ready():
 #	pass
 
 func _physics_process(delta):
-	# print(Input.get_axis("down_thrust", "up_thrust"))
-	# add_force(self.get_transform().basis.z * Input.get_axis("down_thrust", "up_thrust") * 20, Vector3(0, 0, 0))
+
+	error = desired_drone_state.translation.y - self.translation.y
+	error_derivative = (error - previous_error) / delta
+	previous_error = error
+	error_integral = error_integral + error * delta
+
+	thrust = PGAIN * error + DGAIN * error_derivative + IGAIN * error_integral
+
+	motorFR = thrust + yaw + pitch + roll
+	motorFL = thrust - yaw + pitch - roll
+	motorBR = thrust - yaw - pitch + roll
+	motorBL = thrust + yaw - pitch - roll
 	
-	add_force(self.get_transform().basis.y * Input.get_axis("down_thrust", "up_thrust") * 10, $Proto_Drone_Mesh/P1.get_transform().origin)
-	add_force(self.get_transform().basis.y * Input.get_axis("down_thrust", "up_thrust") * 10, $Proto_Drone_Mesh/P2.get_transform().origin)
-	add_force(self.get_transform().basis.y * Input.get_axis("down_thrust", "up_thrust") * 10, $Proto_Drone_Mesh/P3.get_transform().origin)
-	add_force(self.get_transform().basis.y * Input.get_axis("down_thrust", "up_thrust") * 10, $Proto_Drone_Mesh/P4.get_transform().origin)
+	add_force(self.get_transform().basis.y * motorFL * SPEED, $Proto_Drone_Mesh/P1.get_transform().origin)
+	add_force(self.get_transform().basis.y * motorBL * SPEED, $Proto_Drone_Mesh/P2.get_transform().origin)
+	add_force(self.get_transform().basis.y * motorBR * SPEED, $Proto_Drone_Mesh/P3.get_transform().origin)
+	add_force(self.get_transform().basis.y * motorFR * SPEED, $Proto_Drone_Mesh/P4.get_transform().origin)
 	
-	add_torque(Vector3(0, Input.get_axis("yaw_right", "yaw_left") * 10, 0))
+	# add_torque(Vector3(0, Input.get_axis("yaw_right", "yaw_left") * 10, 0))
+
+	pass
